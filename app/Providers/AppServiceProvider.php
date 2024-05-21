d45c43deeb27f6392e1933111ce34cbdef93b5a3
Hugo Henrique Fumero de Souza <hugohfsouza@hotmail.com>
2024-05-21 18:19:47 -0300

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,8 +19,9 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
         Schema::defaultStringLength(191);
+        $url->forceScheme('https');
     }
 }
